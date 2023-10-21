97c61694a16d51727a8178fd44c2087c8b4fb2f7
Agustin Burset <agustin.burset@icloud.com>
2023-10-21 08:54:43 -0300

    disable default code styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  markdown: { syntaxHighlight: false },
 })
