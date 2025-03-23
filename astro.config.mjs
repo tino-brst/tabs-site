f048ae3d742e3ed8b271eab8821be645f7a2be31
Agustin Burset <agustin.burset@icloud.com>
2025-03-23 14:19:34 -0300

    upgrade to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel'
 import rehypeHighlight from 'rehype-highlight'
 import { tab } from './src/lib/tab'
 
